d3c35e34f2730f283561a53573f81325f0adcc24
skrutosha00 <istov00@bk.ru>
2023-03-13 15:40:11 +0300

    remake

--- scripts/main.js
+++ scripts/main.js
@@ -1,53 +1,59 @@
 import { setBalanceField } from "./functions.js";
 
-if (!localStorage.getItem('balance_snake')) {
-    localStorage.setItem('balance_snake', 100)
+if (!localStorage.getItem("balance_snake")) {
+  localStorage.setItem("balance_snake", 100);
 }
 
-if (!localStorage.getItem('levels_snake')) {
-    localStorage.setItem('levels_snake', [1])
+if (!localStorage.getItem("levels_snake")) {
+  localStorage.setItem("levels_snake", [1]);
 }
 
-if (!localStorage.getItem('1_snake')) {
-    localStorage.setItem('1_snake', 1)
+if (!localStorage.getItem("1_snake")) {
+  localStorage.setItem("1_snake", 1);
 }
 
-if (!localStorage.getItem('chosen_snake')) {
-    localStorage.setItem('chosen_snake', 1)
+if (!localStorage.getItem("chosen_snake")) {
+  localStorage.setItem("chosen_snake", 1);
 }
 
-if (!localStorage.getItem('fruit_snake')) {
-    localStorage.setItem('fruit_snake', 0)
+if (!localStorage.getItem("fruit_snake")) {
+  localStorage.setItem("fruit_snake", 0);
 }
 
-if (!localStorage.getItem('rewards_snake')) {
-    localStorage.setItem('rewards_snake', [])
+if (!localStorage.getItem("rewards_snake")) {
+  localStorage.setItem("rewards_snake", []);
 }
 
-setBalanceField()
+setBalanceField();
 
-document.querySelector('.wrapper').style.background = 'url(../png/back_' + localStorage.getItem('chosen_snake') + '.png) center/100% 100%'
+document.querySelector(".wrapper").style.background =
+  "url(./png/back_" +
+  localStorage.getItem("chosen_snake") +
+  ".png) center/100% 100%";
 
 for (let i = 0; i < 6; i++) {
-    let levels = Array.from(localStorage.getItem('levels_snake').split(','), el => Number(el))
+  let levels = Array.from(
+    localStorage.getItem("levels_snake").split(","),
+    (el) => Number(el)
+  );
 
-    let level = document.createElement('a')
-    level.classList.add('level', 'block')
-    level.innerHTML = i + 1
+  let level = document.createElement("a");
+  level.classList.add("level", "block");
+  level.innerHTML = i + 1;
 
-    if (!(levels.includes(i + 1))) {
-        level.classList.add('disabled')
-    } else {
-        level.href = './game.html'
+  if (!levels.includes(i + 1)) {
+    level.classList.add("disabled");
+  } else {
+    level.href = "./layouts/game.html";
 
-        level.onclick = () => {
-            localStorage.setItem('level_snake', i + 1)
-        }
-    }
+    level.onclick = () => {
+      localStorage.setItem("level_snake", i + 1);
+    };
+  }
 
-    document.querySelector('.level_cont').append(level)
+  document.querySelector(".level_cont").append(level);
 }
 
 window.onload = () => {
-    document.querySelector('.wrapper').classList.remove('hidden')
-}
+  document.querySelector(".wrapper").classList.remove("hidden");
+};
--- scripts/shop.js
+++ scripts/shop.js
@@ -1,79 +1,86 @@
-import { setBalanceField, changeBalance, animateOnce } from "./functions.js"
+import { setBalanceField, changeBalance, animateOnce } from "./functions.js";
 
 let items = [
-    { name: "1", price: 0 },
-    { name: "2", price: 150 },
-    { name: "3", price: 250 }
-]
+  { name: "1", price: 0 },
+  { name: "2", price: 150 },
+  { name: "3", price: 250 }
+];
 
-setBalanceField()
-let balance = document.querySelector('.balance')
-let cardCont = document.querySelector('.shop')
+setBalanceField();
+let balance = document.querySelector(".balance");
+let cardCont = document.querySelector(".shop");
 
-document.querySelector('.wrapper').style.background = 'url(../png/back_' + localStorage.getItem('chosen_snake') + '.png) center/100% 100%'
+document.querySelector(".wrapper").style.background =
+  "url(../png/back_" +
+  localStorage.getItem("chosen_snake") +
+  ".png) center/100% 100%";
 
 for (let item of items) {
+  if (!localStorage.getItem(item.name + "_snake")) {
+    localStorage.setItem(item.name + "_snake", 0);
+  }
 
-    if (!localStorage.getItem(item.name + '_snake')) {
-        localStorage.setItem(item.name + '_snake', 0)
-    }
+  let card = document.createElement("div");
+  card.classList.add("card");
 
-    let card = document.createElement('div')
-    card.classList.add('card')
+  let picCont = document.createElement("div");
+  picCont.classList.add("pic_cont", "block");
+  card.appendChild(picCont);
 
-    let picCont = document.createElement('div')
-    picCont.classList.add('pic_cont', 'block')
-    card.appendChild(picCont)
+  let pic = document.createElement("img");
+  pic.src = "../png/shop_" + item.name + ".png";
+  picCont.appendChild(pic);
 
-    let pic = document.createElement('img')
-    pic.src = '../png/shop_' + item.name + '.png'
-    picCont.appendChild(pic)
+  let button = document.createElement("div");
+  button.classList.add("button", "block");
+  button.innerHTML = item.price;
+  button.dataset.item = item.name;
 
-    let button = document.createElement('div')
-    button.classList.add('button', 'block')
-    button.innerHTML = item.price
-    button.dataset.item = item.name
+  if (localStorage.getItem(item.name + "_snake") == 1) {
+    button.innerHTML = "Select";
+  }
 
-    if (localStorage.getItem(item.name + '_snake') == 1) {
-        button.innerHTML = 'Select'
-    }
+  if (localStorage.getItem("chosen_snake") == item.name) {
+    button.innerHTML = "Selected";
+  }
 
-    if (localStorage.getItem('chosen_snake') == item.name) {
-        button.innerHTML = 'Selected'
-    }
+  card.appendChild(button);
 
-    card.appendChild(button)
+  button.onclick = () => {
+    let price = item.price;
 
-    button.onclick = () => {
-        let price = item.price
+    if (localStorage.getItem(button.dataset.item + "_snake") == 1) {
+      localStorage.setItem("chosen_snake", button.dataset.item);
 
-        if (localStorage.getItem(button.dataset.item + '_snake') == 1) {
-            localStorage.setItem('chosen_snake', button.dataset.item)
-
-            for (let b of document.querySelectorAll('.button')) {
-                if (b.innerHTML == 'Selected') {
-                    b.innerHTML = 'Select'
-                }
-            }
-
-            document.querySelector('.wrapper').style.background = 'url(../png/back_' + localStorage.getItem('chosen_snake') + '.png) center/100% 100%'
-            button.innerHTML = 'Selected'
+      for (let b of document.querySelectorAll(".button")) {
+        if (b.innerHTML == "Selected") {
+          b.innerHTML = "Select";
         }
+      }
 
-        if (button.innerHTML == price) {
-            if (Number(balance.innerHTML) <= price) { return }
-
-            button.innerHTML = 'Select'
-            changeBalance(-price)
-
-            localStorage.setItem(button.dataset.item + '_snake', 1)
-            localStorage.setItem('chosen_snake', button.dataset.item)
-        }
+      document.querySelector(".wrapper").style.background =
+        "url(../png/back_" +
+        localStorage.getItem("chosen_snake") +
+        ".png) center/100% 100%";
+      button.innerHTML = "Selected";
     }
 
-    cardCont.appendChild(card)
+    if (button.innerHTML == price) {
+      if (Number(balance.innerHTML) <= price) {
+        return;
+      }
+
+      button.innerHTML = "Select";
+      changeBalance(-price);
+
+      localStorage.setItem(button.dataset.item + "_snake", 1);
+      localStorage.setItem("chosen_snake", button.dataset.item);
+    }
+  };
+
+  cardCont.appendChild(card);
 }
 
 window.onload = () => {
-    document.querySelector('.wrapper').classList.remove('hidden')
-}
+  document.querySelector(".wrapper").classList.remove("hidden");
+};
